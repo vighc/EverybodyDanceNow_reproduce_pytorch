19d25f92bc2f80857a7b977f276270107f934969
vighc <61262139+vighc@users.noreply.github.com>
2020-05-09 19:22:55 +0530

    Update visualizer.py

--- src/pix2pixHD/util/visualizer.py
+++ src/pix2pixHD/util/visualizer.py
@@ -4,8 +4,8 @@ import numpy as np
 import os
 import ntpath
 import time
-from src.pix2pixHD.util import util
-from src.pix2pixHD.util import html
+from pix2pixHD.util import util
+from pix2pixHD.util import html
 import scipy.misc
 try:
     from StringIO import StringIO  # Python 2.7
