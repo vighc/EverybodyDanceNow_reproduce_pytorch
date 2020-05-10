d5b0397eab0324dcff6a6e807acdf2200182d88e
vighc <61262139+vighc@users.noreply.github.com>
2020-05-10 09:58:14 +0530

    Update models.py

--- src/pix2pixHD/models/models.py
+++ src/pix2pixHD/models/models.py
@@ -4,7 +4,7 @@ import torch
 
 def create_model(opt):
     if opt.model == 'pix2pixHD':
-        from pix2pixHD_model import Pix2PixHDModel, InferenceModel
+        from .pix2pixHD_model import Pix2PixHDModel, InferenceModel
         if opt.isTrain:
             model = Pix2PixHDModel()
         else:
